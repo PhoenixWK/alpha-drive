cfdd2f388432daf8c88aa0cb3a227b36842f53f4
xuanphuda <xuanphuda@gmail.com>
2025-05-19 15:00:44 +0700

    Initial commit with backend and frontend

---
+++ backend/alpha-drive/src/test/java/com/file_storage/alpha_drive/AlphaDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.file_storage.alpha_drive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

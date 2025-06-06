6ae73d3fc43b93c8cbfa03abdb23e40050a8e907
xuanphuda <xuanphuda@gmail.com>
2025-06-06 20:06:11 +0700

    initialize project

---
+++ backend/alpha-drive/src/main/java/com/file_storage/alpha_drive/AlphaDriveApplication.java
@@ -0,0 +1,13 @@
+package com.file_storage.alpha_drive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlphaDriveApplication.class, args);
+	}
+
+}
